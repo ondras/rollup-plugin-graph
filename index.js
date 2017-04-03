d78617f87d9f0a601339e923776c75c37f53c893
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2017-04-03 10:50:16 +0200

    first version

---
+++ index.js
@@ -0,0 +1,52 @@
+function toDot(modules) {
+	console.log("digraph G {");
+	console.log("edge [dir=back]")
+	modules.forEach(m => {
+		m.deps.forEach(dep => {
+			console.log(`"${dep}" -> "${m.id}"`);
+		});
+	});
+	console.log("}");
+}
+
+function prune(modules) {
+	let avail = modules.filter(m => m.deps.length == 0);
+	if (!avail.length) { return; }
+
+	let id = avail[0].id;
+//    console.log("pruning", id);
+	let index = modules.indexOf(avail[0]);
+	modules.splice(index, 1);
+	modules.forEach(m => {
+		m.deps = m.deps.filter(dep => dep != id);
+	});
+	prune(modules);
+}
+
+function getPrefix(ids) {
+	if (ids.length < 2) { return ""; }
+	return ids.reduce((prefix, val) => {
+		while (val.indexOf(prefix) != 0) { prefix = prefix.substring(0, prefix.length-1); }
+		return prefix;
+	});
+}
+
+module.exports = function plugin(options = {}) {
+	return {
+		onwrite(data) {
+			let ids = data.bundle.modules.map(m => m.id);
+			let prefix = getPrefix(ids);
+
+			let modules = [];
+			data.bundle.modules.forEach(module => {
+				let m = {
+					id: module.id.substring(prefix.length),
+					deps: module.dependencies.map(dep => dep.substring(prefix.length))
+				}
+				modules.push(m);
+			});
+			if (options.prune) { prune(modules); }
+			toDot(modules);
+		}
+	}
+}
